20e4f16a5a8ba7827bd14a100cd14f4f0437dbbc
Spartak <spartaklysman@gmail.com>
2024-01-03 15:00:35 +0100

    Spring course. Updated import

--- SchoolApplication/src/test/java/ua/com/foxminded/javaspring/SchoolApplication/JDBCStudentDaoTest.java
+++ SchoolApplication/src/test/java/ua/com/foxminded/javaspring/SchoolApplication/JDBCStudentDaoTest.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
