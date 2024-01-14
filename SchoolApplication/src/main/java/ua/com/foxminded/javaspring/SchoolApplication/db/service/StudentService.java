2a9aba25c00f043e89fa1bcb063f0d0fe86b76c9
Spartak <spartaklysman@gmail.com>
2024-01-14 16:25:14 +0100

    Spring course. Logger updated

--- SchoolApplication/src/main/java/ua/com/foxminded/javaspring/SchoolApplication/db/service/StudentService.java
+++ SchoolApplication/src/main/java/ua/com/foxminded/javaspring/SchoolApplication/db/service/StudentService.java
@@ -2,19 +2,35 @@ package ua.com.foxminded.javaspring.SchoolApplication.db.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import ua.com.foxminded.javaspring.SchoolApplication.db.impl.postgre.PostgreSqlStudentDao;
 import ua.com.foxminded.javaspring.SchoolApplication.model.Entity;
 import ua.com.foxminded.javaspring.SchoolApplication.model.Student;
 import ua.com.foxminded.javaspring.SchoolApplication.model.User;
+import ua.com.foxminded.javaspring.SchoolApplication.util.LoggingController;
 
 @Service
 public class StudentService {
 
 	@Autowired
 	private PostgreSqlStudentDao studentRepository;
+
+	Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+	@RequestMapping("/")
+	public String index() {
+		logger.debug("A DEBUG Message");
+		logger.info("An INFO Message");
+		logger.warn("A WARN Message");
+		logger.error("An ERROR Message");
+
+		return "Student Service";
+	}
 
 	public boolean create(Student student) {
 		return studentRepository.create(student);
